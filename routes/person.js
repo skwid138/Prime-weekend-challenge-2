ca2f8d0205f8cfe732eaf48ce1b1d11d7238cdd2
Hunter Rancourt <skwid138@gmail.com>
2017-09-09 10:16:40 -0500

    server logs each POST object and adds it to a global array

--- routes/person.js
+++ routes/person.js
@@ -1,6 +1,9 @@
 // requires
 var router = require('express').Router();
 
+// global array of people
+var personArray = [];
+
 // router
 router.get('/', function (reg, res) {
     console.log('in get person route');
@@ -8,7 +11,10 @@ router.get('/', function (reg, res) {
 });
 
 router.post('/', function (req, res) {
-    console.log('in post person route', req.body);
+    console.log('person POST route hit: ', req.body);
+    personArray.push(req.body);
+    console.log('personArray ->', personArray);
+    
     res.sendStatus(201);
 });
 
