b52758854d706a90dc2fc6a86a98ce15ffcdd24a
Hunter Rancourt <skwid138@gmail.com>
2017-09-09 09:48:47 -0500

    created person route and tested get

--- routes/person.js
+++ routes/person.js
@@ -1,5 +1,16 @@
 // requires
+var router = require('express').Router();
 
 // router
+router.get('/', function (reg, res) {
+    console.log('in get person route');
+    res.send('person route');
+});
+
+router.post('/', function (req, res) {
+    console.log('in post person route', req.body);
+    res.sendStatus(201);
+});
 
 // export
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index'); // for base route
+var personRouter = require('./routes/person'); // for person route
 
 // globals
 var app = express();
@@ -14,6 +15,7 @@ app.use(express.static('public')); // sets source folder for client side
 
 // routes
 app.use('/', indexRouter);
+app.use('/person', personRouter);
 
 //listen
 app.listen(port, function () {
