5ecbe6edfb1a2c548d2453ca230d55415cf929ad
Hunter Rancourt <skwid138@gmail.com>
2017-09-09 10:47:52 -0500

    added buttons for carousel, added listener for carousel, created carousel function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,6 +9,9 @@ function onReady( ) {
 
     // listener for addPersonButton
     $('#addPersonButton').on('click', addPerson);
+
+    // listener for carousel
+    $('#peopleSection').on('click', carousel);
 }
 
 function addPerson( ) {
@@ -51,10 +54,18 @@ function getPeople( ) {
             for (var i = 0; i < response.length; i++) {
                 $div.append('<p>');
                 $div.append(response[i].name + ': ' + response[i].fact);
+                $div.append('<button id="prevButton">Prev</button>'); // buttons for carousel
+                $div.append('<button id="nextButton">Next</button>'); // buttons for carousel
             }
             $('#peopleSection').append($div);
         }
     });
+}
+
+function carousel( ) {
+//Create a carousel that shows only one person at a time. 
+// There should be "next" and "previous" buttons that wrap
+//and an indicator of which person is being shown:
 }
 
 
